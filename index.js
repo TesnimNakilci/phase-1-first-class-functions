1a9e14038bc1db81cd532716deb1d1cb403293b7
TesnimNakilci <115715881+TesnimNakilci@users.noreply.github.com>
2023-03-29 15:23:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function funcc(){
+  
+    }
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+  
+    }
+  }
